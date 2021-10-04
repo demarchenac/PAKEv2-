2594042f52c7ffb94b6da56c7431d6209794db4a
demarchenac <demarchenac@uninorte.edu.co>
2021-10-04 14:47:28 -0500

    Chore: Added Black code formatter

--- SocketClient.py
+++ SocketClient.py
@@ -6,138 +6,136 @@ from Parameters import Parameters
 from EncondingHelper import EncodingHelper
 from Constants import AUTH, PARAMETERS, SERVER_CONSTANTS
 
+
 class SocketClient:
-    def __init__(self,sock, identifier, password ,host, port, parameters):
+    def __init__(self, sock, identifier, password, host, port, parameters):
         if sock is None:
-            self.sock = socket.socket(
-            socket.AF_INET, socket.SOCK_STREAM)
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         else:
             self.sock = sock
-        self.host=host
-        self.port=port
-        
-        self.password=password
-        self.identifier=identifier
-        self.parameters=parameters
-    
+        self.host = host
+        self.port = port
+
+        self.password = password
+        self.identifier = identifier
+        self.parameters = parameters
+
     def run(self):
         self.connect(self.host, self.port)
-        
-        alpha=random.randint(1,self.parameters.q-1)
-        U1=self.parameters.G.point_multiplication(alpha)
-        
-        pw=bytes(self.password,'utf-8')
-        K=self.parameters.get_k(pw)
-        U2=self.parameters.A.point_multiplication(K)
-        
-        U=U1+U2
-        
-        ubytes=U.to_bytes()
-        id_p=bytes(self.identifier,'utf-8')
-        
-        L=[ubytes,id_p]
-       
-        array=EncodingHelper.encodeArray(L) 
-        
-        
-        array=EncodingHelper.encodeArray([array])
-        
-        
+
+        alpha = random.randint(1, self.parameters.q - 1)
+        U1 = self.parameters.G.point_multiplication(alpha)
+
+        pw = bytes(self.password, "utf-8")
+        K = self.parameters.get_k(pw)
+        U2 = self.parameters.A.point_multiplication(K)
+
+        U = U1 + U2
+
+        ubytes = U.to_bytes()
+        id_p = bytes(self.identifier, "utf-8")
+
+        L = [ubytes, id_p]
+
+        array = EncodingHelper.encodeArray(L)
+
+        array = EncodingHelper.encodeArray([array])
+
         self.send(array)
-        
-        arrayRec=self.receive()
-        
-        L=EncodingHelper.decodeArray(arrayRec)
-        
-        vbytes=L[0]
-        id_q=L[1]
-        
-        V=ECPoint.point_from_bytes(self.parameters.a,self.parameters.b,vbytes)
-        
-        V2=self.parameters.B.point_multiplication(K)
-        
-        W=(V-V2).point_multiplication(alpha)
-        
-        wbytes=W.to_bytes()
-        
-        keyblob=self.parameters.H(pw,id_p,id_q,ubytes, vbytes,wbytes, 45)
-        
-        key =keyblob[:32]
+
+        arrayRec = self.receive()
+
+        L = EncodingHelper.decodeArray(arrayRec)
+
+        vbytes = L[0]
+        id_q = L[1]
+
+        V = ECPoint.point_from_bytes(self.parameters.a, self.parameters.b, vbytes)
+
+        V2 = self.parameters.B.point_multiplication(K)
+
+        W = (V - V2).point_multiplication(alpha)
+
+        wbytes = W.to_bytes()
+
+        keyblob = self.parameters.H(pw, id_p, id_q, ubytes, vbytes, wbytes, 45)
+
+        key = keyblob[:32]
         nonce = keyblob[32:]
-        mask=int('0xffffffffffffffffffffffffff', base=16)
-        #print(keyblob.hex())
-        vnonce=int.from_bytes(nonce, "big")
-        cont=True
+        mask = int("0xffffffffffffffffffffffffff", base=16)
+        # print(keyblob.hex())
+        vnonce = int.from_bytes(nonce, "big")
+        cont = True
         while cont:
             print("Escriba un mensaje para enviar: Si escribe 'exit' finaliza")
-            message=input()
-            
-            data=bytes(message,'utf-8')
-            nonce=vnonce.to_bytes(13, byteorder='big')
+            message = input()
+
+            data = bytes(message, "utf-8")
+            nonce = vnonce.to_bytes(13, byteorder="big")
             cipher = AES.new(key, AES.MODE_GCM, nonce=nonce)
             ciphertext, tag = cipher.encrypt_and_digest(data)
             print(ciphertext)
-            vnonce=(vnonce+1)& mask
-            array=EncodingHelper.encodeArray([tag+ciphertext])
+            vnonce = (vnonce + 1) & mask
+            array = EncodingHelper.encodeArray([tag + ciphertext])
             self.send(array)
-            
-            if message=='exit':
-                cont=False
-        
-        
+
+            if message == "exit":
+                cont = False
+
         self.sock.close()
-      
-  
+
     def connect(self, host, port):
         self.sock.connect((host, port))
-        #c=self.receive()
-        
-  
+        # c=self.receive()
+
     def send(self, msg):
         totalsent = 0
-        msglen=len(msg)
+        msglen = len(msg)
         while totalsent < msglen:
             sent = self.sock.send(msg[totalsent:])
             if sent == 0:
                 self.sock.close()
             totalsent = totalsent + sent
 
-
     def receive(self):
         bytes_recd = 0
         chunk = self.sock.recv(4)
-        if chunk == b'':
-                self.sock.close()
-        
+        if chunk == b"":
+            self.sock.close()
+
         bytes_recd = 0
-        msglen=int.from_bytes(chunk, byteorder='big')
+        msglen = int.from_bytes(chunk, byteorder="big")
         chunks = []
         while bytes_recd < msglen:
             chunk = self.sock.recv(min(msglen - bytes_recd, 2048))
-            
-            if chunk == b'':
+
+            if chunk == b"":
                 self.sock.close()
             chunks.append(chunk)
             bytes_recd = bytes_recd + len(chunk)
-        
-        
-        return b''.join(chunks)
 
-param = Parameters(
-    PARAMETERS['A']['X'], 
-    PARAMETERS['A']['Y'], 
-    PARAMETERS['B']['X'], 
-    PARAMETERS['B']['Y']
-)
+        return b"".join(chunks)
 
-client = SocketClient(
-    None, 
-    AUTH['USER'], 
-    AUTH['PASSWORD'], 
-    SERVER_CONSTANTS['HOST'], 
-    SERVER_CONSTANTS['PORT'], 
-    param
-)
 
-client.run()
+def startSocketClient():
+    param = Parameters(
+        PARAMETERS["A"]["X"],
+        PARAMETERS["A"]["Y"],
+        PARAMETERS["B"]["X"],
+        PARAMETERS["B"]["Y"],
+    )
 
+    client = SocketClient(
+        None,
+        AUTH["USER"],
+        AUTH["PASSWORD"],
+        SERVER_CONSTANTS["HOST"],
+        SERVER_CONSTANTS["PORT"],
+        param,
+    )
+
+    client.run()
+
+
+if __name__ == "__main__":
+    startSocketClient()
--- SocketServer.py
+++ SocketServer.py
@@ -6,92 +6,90 @@ from Parameters import Parameters
 from EncondingHelper import EncodingHelper
 from Constants import DATABASE, PARAMETERS, SERVER_CONSTANTS, SOCKET_CONSTANTS
 
-class client(Thread):  
-    def __init__(self, socket, address,identifier, parameters):
+
+class client(Thread):
+    def __init__(self, socket, address, identifier, parameters):
         Thread.__init__(self)
         self.sock = socket
         self.addr = address
-        self.identifier=identifier
-        self.parameters=parameters
+        self.identifier = identifier
+        self.parameters = parameters
         self.start()
-    
+
     def retrieve(self, ID):
         if ID in DATABASE.keys():
             return DATABASE[ID]
         else:
-            raise ValueError('Error con id')
-      
+            raise ValueError("Error con id")
+
     def run(self):
         try:
-            response=self.receive()
-            
-            L=EncodingHelper.decodeArray(response)
-            
-            ubytes=L[0]
-            id_p=L[1]
-            id_ps=id_p.decode('utf-8')
-            
-            pwst=self.retrieve(id_ps)
-            
-            
-            
-            pw=bytes(pwst,'utf-8')
-            
-            K=self.parameters.get_k(pw)
-            
-            beta=random.randint(1,self.parameters.q-1)
-            
-            V1=self.parameters.G.point_multiplication(beta)
-            V2=self.parameters.B.point_multiplication(K)
-            
-            V=V1+V2
-            
-            vbytes=V.to_bytes()
-            id_q=bytes(self.identifier,'utf-8')
-            
-            L=[vbytes,id_q]
-            
-            array= EncodingHelper.encodeArray(L) 
-            
-            self.send(EncodingHelper.encodeArray([array]) )
-        
-            
-            U2=self.parameters.A.point_multiplication(K)
-            
-            U=ECPoint.point_from_bytes(self.parameters.a,self.parameters.b,ubytes)
-            
-            W=(U-U2).point_multiplication(beta)
-            
-            wbytes=W.to_bytes()
-            
-            keyblob=self.parameters.H(pw,id_p,id_q, ubytes, vbytes, wbytes, 45)
-            
-            key =keyblob[:32]
+            response = self.receive()
+
+            L = EncodingHelper.decodeArray(response)
+
+            ubytes = L[0]
+            id_p = L[1]
+            id_ps = id_p.decode("utf-8")
+
+            pwst = self.retrieve(id_ps)
+
+            pw = bytes(pwst, "utf-8")
+
+            K = self.parameters.get_k(pw)
+
+            beta = random.randint(1, self.parameters.q - 1)
+
+            V1 = self.parameters.G.point_multiplication(beta)
+            V2 = self.parameters.B.point_multiplication(K)
+
+            V = V1 + V2
+
+            vbytes = V.to_bytes()
+            id_q = bytes(self.identifier, "utf-8")
+
+            L = [vbytes, id_q]
+
+            array = EncodingHelper.encodeArray(L)
+
+            self.send(EncodingHelper.encodeArray([array]))
+
+            U2 = self.parameters.A.point_multiplication(K)
+
+            U = ECPoint.point_from_bytes(self.parameters.a, self.parameters.b, ubytes)
+
+            W = (U - U2).point_multiplication(beta)
+
+            wbytes = W.to_bytes()
+
+            keyblob = self.parameters.H(pw, id_p, id_q, ubytes, vbytes, wbytes, 45)
+
+            key = keyblob[:32]
             nonce = keyblob[32:]
-            mask=int('0xffffffffffffffffffffffffff', base=16)
-            vnonce=int.from_bytes(nonce, "big")
-            cont=True
-            
+            mask = int("0xffffffffffffffffffffffffff", base=16)
+            vnonce = int.from_bytes(nonce, "big")
+            cont = True
+
             while cont:
-                data=self.receive()
-                try: 
-                    nonce=vnonce.to_bytes(13, byteorder='big')
+                data = self.receive()
+                try:
+                    nonce = vnonce.to_bytes(13, byteorder="big")
                     cipher = AES.new(key, AES.MODE_GCM, nonce=nonce)
-                    plaintext = cipher.decrypt_and_verify(data[16:],data[:16]) 
-                    vnonce=(vnonce+1)& mask
-                    print(plaintext.decode('utf-8'))
-                    if plaintext=='exit':
-                        cont=False
+                    plaintext = cipher.decrypt_and_verify(data[16:], data[:16])
+                    vnonce = (vnonce + 1) & mask
+                    print(plaintext.decode("utf-8"))
+                    if plaintext == "exit":
+                        cont = False
                 except:
                     raise RuntimeError("Encryption Error")
-            self.sock.close() 
-            
+            self.sock.close()
+
         except:
-            self.sock.close()    
-            
+            self.sock.close()
+
     def send(self, msg):
         totalsent = 0
-        msglen=len(msg)
+        msglen = len(msg)
         while totalsent < msglen:
             sent = self.sock.send(msg[totalsent:])
             if sent == 0:
@@ -101,49 +99,51 @@ class client(Thread):
     def receive(self):
         bytes_recd = 0
         chunk = self.sock.recv(4)
-        if chunk == b'':
-                self.sock.close()
-        
+        if chunk == b"":
+            self.sock.close()
+
         bytes_recd = 0
-        msglen=int.from_bytes(chunk, byteorder='big')
+        msglen = int.from_bytes(chunk, byteorder="big")
         chunks = []
 
         while bytes_recd < msglen:
             chunk = self.sock.recv(min(msglen - bytes_recd, 2048))
-            
-            if chunk == b'':
+
+            if chunk == b"":
                 self.sock.close()
             chunks.append(chunk)
             bytes_recd = bytes_recd + len(chunk)
-        
-        return b''.join(chunks)
+
+        return b"".join(chunks)
+
 
 def startSocketServer():
-    
+
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serversocket.bind((SERVER_CONSTANTS['HOST'], SERVER_CONSTANTS['PORT']))
-    serversocket.listen(SERVER_CONSTANTS['POOL_SIZE'])
-    print('server started and listening')
-    
+    serversocket.bind((SERVER_CONSTANTS["HOST"], SERVER_CONSTANTS["PORT"]))
+    serversocket.listen(SERVER_CONSTANTS["POOL_SIZE"])
+    print("server started and listening")
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(('', SOCKET_CONSTANTS['PORT']))
-    s.listen(SOCKET_CONSTANTS['CON_SIZE'])
+    s.bind(("", SOCKET_CONSTANTS["PORT"]))
+    s.listen(SOCKET_CONSTANTS["CON_SIZE"])
 
     param = Parameters(
-        PARAMETERS['A']['X'], 
-        PARAMETERS['A']['Y'], 
-        PARAMETERS['B']['X'], 
-        PARAMETERS['B']['Y']
+        PARAMETERS["A"]["X"],
+        PARAMETERS["A"]["Y"],
+        PARAMETERS["B"]["X"],
+        PARAMETERS["B"]["Y"],
     )
 
     while True:
         clientsocket, address = serversocket.accept()
         client(
-            socket=clientsocket, 
+            socket=clientsocket,
             address=address,
-            identifier=SERVER_CONSTANTS['IDENTIFIER'],
-            parameters=param
+            identifier=SERVER_CONSTANTS["IDENTIFIER"],
+            parameters=param,
         )
-        
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     startSocketServer()
